bd4a0a8ef78e623fbee136ad9722b9b559ebbb64
Gabriele <gabriele.milan@deltatre.com>
2020-06-25 17:16:39 +0200

    configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ConfigService } from 'src/services/config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  config$ = this.configService.config$;
+
+  constructor(private configService: ConfigService) {
+    configService.loadConfig().subscribe();
+  }
   title = 'angular-docker';
 }
